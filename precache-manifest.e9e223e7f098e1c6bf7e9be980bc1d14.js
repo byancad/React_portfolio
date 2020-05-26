2a1afbc6b08d7264e058e616333b5aa7a551766f
Byanca De la fuente <byancadlf@gmail.com>
2020-05-26 14:48:16 -0500

    Updates

---
+++ precache-manifest.e9e223e7f098e1c6bf7e9be980bc1d14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df8befd5768683823331e2995e9985d",
+    "url": "/React_portfolio/index.html"
+  },
+  {
+    "revision": "96453320f4336797f48c",
+    "url": "/React_portfolio/static/css/main.aeeb6f6f.chunk.css"
+  },
+  {
+    "revision": "931f999d3b6eed30f947",
+    "url": "/React_portfolio/static/js/2.d9720100.chunk.js"
+  },
+  {
+    "revision": "95ffa2987b8824c88940b1d3c5ba19bd",
+    "url": "/React_portfolio/static/js/2.d9720100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96453320f4336797f48c",
+    "url": "/React_portfolio/static/js/main.3d01828a.chunk.js"
+  },
+  {
+    "revision": "09b93e27e5d58ccebbf9",
+    "url": "/React_portfolio/static/js/runtime-main.a2b6ea37.js"
+  },
+  {
+    "revision": "2718500567c576d2ab1afa2d78f6c060",
+    "url": "/React_portfolio/static/media/IMG_6487.27185005.jpeg"
+  }
+]);
